351750ea6db61abfc6d95cdb94189393df8be7cf
Yukifumi <tkhsh.shmd.ykfm.4296@gmail.com>
2020-10-08 22:47:19 +0900

    Initial commit

---
+++ Assets/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+
+    Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < this.cam.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
